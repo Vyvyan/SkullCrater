bd65f918583c04c274382813e8c43b1875eeada6
Trevor Vaughn <TrevorJVaughn@gmail.com>
2016-09-09 18:10:03 -1000

    credit screen, kill zones outside of crater, bonus gold poster, bloop easter egg, removed explosion text

---
+++ Assets/Scripts/KillPlane.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent)
+        {
+            Destroy(other.transform.parent.gameObject);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpecialCrate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialCrate : MonoBehaviour {
+
+    public Rigidbody bloopRB;
+    Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        rb.isKinematic = false;
+        rb.useGravity = true;
+        bloopRB.isKinematic = false;
+        bloopRB.useGravity = true;
+    }
+}
